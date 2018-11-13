8497997c2f02e05a805e1cfe15ed255c1edef500
victoriak94 <vkamila6@gmail.com>
2018-11-13 15:43:00 -0500

    Defined a method returning a boolean if string argument is 8 characters long or not. Used the .length operator within an if else statement to return boolean.

---
+++ exercise4.rb
@@ -0,0 +1,12 @@
+
+def eight_characters_long?(word)
+
+    if word.length < 8
+      return false
+    else
+      return true
+  end
+end
+
+puts eight_characters_long?("Victoria")
+puts eight_characters_long?("John")
